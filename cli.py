952d06bc7941ae490d990e63938e31eb28ae282d
kcouv <kcouvion@cedarville.edu>
2023-03-07 17:12:42 -0500

    init input error messages

--- cli.py
+++ cli.py
@@ -4,13 +4,23 @@
 #   they quoted scripture.
 # Usage:
 #   To analyze a quotation from a text file:
-#     python cli.py <filename>
+#     python cli.py -f <filename>
 #   To analyze a quotation from an input string:
-#     python cli.py "<input_string>"
+#     python cli.py -t "<input_string>"
 
 # Import libraries
 import sys
+import os.path
 
-# Check input according to usage
+# Get arguments
+args = sys.argv
 
+# Verify and sanitize input
+if args.__len__() != 4:
+  print("Usage: python cli.py -f <filename>")
+  print("OR python cli.py -t \"<quotation text>\"")
+  exit()
+elif args[2] == "-f" and not os.path.isfile(args[3]):
+  print("ERROR: File does not exist")
+  exit()
 print("Yay")
