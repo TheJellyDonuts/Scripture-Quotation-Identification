0bb9313055b46cba2fa6c571633b666a863dc9fa
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2023-03-07 10:14:17 -0700

    Abstract tasks to functions

--- prob_analysis.py
+++ prob_analysis.py
@@ -11,15 +11,14 @@ with open('wordList.json', 'r') as f:
     words = f.readlines()
 
 cache = {} # word: [[verse, occurences],[verse, occurences],...]
-# TODO: clear cache at some point? 
 
 versemap = {} # verse: occurences
 not_found = {}
 output = []
 
+# increment the versemap for each verse in verselist
 def inc_versemap(verselist):
     # [verse, occurences]
-    # increment occurences
     # TODO: this doesn't deal with the fact that some words are more common in certain verses
     for verse, occurences in verselist:
         if verse in versemap:
@@ -39,23 +38,30 @@ def add_to_cache(pair):
     word, verselist = pair
     cache.update({word: verselist})
 
+def search_wordlist(word):
+    for word_obj in words:
+        if word_obj.is_variant(word):
+            
+            inc_versemap(word_obj.verse_occurences)
+            add_to_cache(word_obj)
+            return True
+    return False
+
+# go through a clause for each word
 for clause in clauses:
-    # go through a clause for each word
     for word in clause["words"]:
         found = False
 
-        # search cache for the word; if found, increment
+        # search cache for the word; if found, increment respective
+        # verses in versemap
         if check_cache(word):
             found = True
             inc_versemap(cache[word])
 
         # search wordlist & variants for the word
         else:
-            for word_obj in words:
-                if word_obj.is_variant(word):
-                    found = True
-                    inc_versemap(word_obj.verse_occurences)
-                    add_to_cache(word_obj)
+            # iterate through wordlist and add if found
+            found = search_wordlist(word)
 
         # if word not found in cache or wordlist, add to not_found
         # list. Keep track of occurences
@@ -66,7 +72,7 @@ for clause in clauses:
                 not_found[word] += 1
 
     # TODO: deal with punctuation
-    
+
     # add data to output and clear cache
     output += [clause["line_number"], versemap]
     cache.clear()
