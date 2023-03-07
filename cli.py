8ed31760331d47add902e0829d75ce30a9e90fa6
kcouv <kcouvion@cedarville.edu>
2023-03-07 17:04:10 -0500

    creation & light documentation

---
+++ cli.py
@@ -0,0 +1,16 @@
+# Purpose:
+#   Command-Line Interface for analysing a greek
+#   quotation from a church father to see where
+#   they quoted scripture.
+# Usage:
+#   To analyze a quotation from a text file:
+#     python cli.py <filename>
+#   To analyze a quotation from an input string:
+#     python cli.py "<input_string>"
+
+# Import libraries
+import sys
+
+# Check input according to usage
+
+print("Yay")
